061b7432d003a3530bc070f285e26d9f3b801efa
Fabiano <fabiano.ssantos79@gmail.com>
2024-01-29 03:08:04 -0300

    codigos

--- src/ListagemProdutos.java
+++ src/ListagemProdutos.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ListagemProdutos {
+public class ListagemProdutos {
+    public static void main(String[] args) {
+        String produto1 = "Livro";
+        String produto2 = "Celular";
+        double preco1 = 49.95;
+        double preco2 = 1470.76;
+        String resultado = """
+                    >>> Listagem de Produtos
+                    ________________________
+                    Nome: %s
+                    Preço: %.2f
+                    ________________________
+                    Nome: %s
+                    Preço: %.2f
+                """.formatted(produto1, preco1, produto2, preco2);
+        System.out.println(resultado);
+    }
 }
